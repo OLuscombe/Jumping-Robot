62ee79c4433d3d3ef9321e2cf9597d8ae689b0c1
OLuscombe <63856980+OLuscombe@users.noreply.github.com>
2023-11-22 16:31:10 +0000

    Add files via upload

--- MDM2_2D.m
+++ MDM2_2D.m
@@ -1,26 +1,31 @@
-c_d =0.47;      % drag coefficent (0.47 for sphere) 
-area=0.01;      % incident area to air flow
-mass=1;     %total mass of the robot
+c_d =1;      % drag coefficent (0.47 for sphere) 
+area=1;      % incident area to air flow
+mass=10;     %total mass of the robot
 angles = linspace(0.1, pi/2, 100);
-initialVel = 5;
+initialVel = 11.54;
 
 xMaxList = [];
 
 for i =1 : length(angles)
     currAngle = angles(i);
-    v0 = [initialVel*cos(currAngle), initialVel*sin(currAngle)]
-    [sxMax, syMax, vx_final, vy_final, v_final, curve, sx, sy] = coupledAirResistanceSolver(v0, c_d, area, mass);
+    v0 = [initialVel*cos(currAngle), initialVel*sin(currAngle), 0, 0];
+
+    [sxMax, syMax, vx_final, vy_final, v_final, sx, sy] = coupledAirResistanceSolver(v0, c_d, area, mass);
     xMaxList = [xMaxList, sxMax];
 end
 
 bestDistance = max(xMaxList)
 indice = find(xMaxList ==  max(xMaxList));
-bestAngle = angles(indice);
-v0 = [initialVel*cos(bestAngle), initialVel*sin(bestAngle)];
-
-angleDegrees = rad2deg(bestAngle)
-[sxMax, syMax, vx_final, vy_final, v_final, curve, sx, sy] = coupledAirResistanceSolver(v0, c_d, area, mass);
+bestAngle = angles(indice)
+v0 = [initialVel*cos(bestAngle), initialVel*sin(bestAngle), 0, 0];
+[sxMax, syMax, vx_final, vy_final, v_final, sx, sy] = coupledAirResistanceSolver(v0, c_d, area, mass);
 plot(sx, sy, 'LineWidth', 2, 'Color', '#0072BD'), hold off;
+xlabel("Horizontal Displacement");
+ylabel("Vertical Displacement");
+title("Displacement Graph");
+v_final
+syMax
+rad2deg(bestAngle) 
 grid on;
 ylim([0, 5])
 axis("equal")
--- coupledAirResistanceSolver.m
+++ coupledAirResistanceSolver.m
@@ -1,28 +1,33 @@
-function[sxMax, syMax, vx_final, vy_final, v_final, curve, sx, sy] = coupledAirResistanceSolver(v0, c_d, area, mass)
+function[sxMax, syMax, vx_final, vy_final, v_final, sx, sy] = coupledAirResistanceSolver(v0, c_d, area, mass)
 
 air_density=1.225;
 g=9.81;
-tspan = [0, 10];
-
+tspan = [0, 50];
 alpha=air_density*c_d*area;
 
+
 f = @(t, v) [-alpha/mass*sqrt(v(1)^2+v(2)^2)*v(1);  %define system of equations
-    -alpha/mass*sqrt(v(1)^2+v(2)^2)*v(2)-g];
-options = odeset('RelTol', 1e-10, 'AbsTol', 1e-12);
+    -alpha/mass*sqrt(v(1)^2+v(2)^2)*v(2)-g; v(1); v(2)];
+
+options = odeset('RelTol', 1e-10, 'AbsTol', 1e-12,'Events', @event_function);
 
 [t, y] = ode45(f, tspan, v0, options);
-curve = [t, y];
+sx = y(:, 3);
+sy = y(:, 4);
+syMax = y(end, 4);
+sxMax = y(end, 3);
 
-sx = cumtrapz(t, y(:, 1));  % cumulative intergral of horizontal velocity 
-sy = cumtrapz(t, y(:, 2));  % cumulative intergral of vertical velocity 
 
-syMax = max(sy);
-indice = find(sy < 0, 1);  % find where robot reaches the floor
-sxMax =sx(indice);        % find corrospodnig horozontal distance traveled
+vx_final = y(end, 1)
+vy_final = y(end, 2)
+v_final = sqrt(vx_final.^2+vy_final.^2)
 
-vx_final = y(indice, 1);
-vy_final = y(indice, 2);
-v_final = sqrt(vx_final^2+vy_final^2);
+end
 
+function [value, isterminal, direction] = event_function(t, y)
+    % Define the event function
+    value = y(4);  % Stop when y reaches 0.5
+    isterminal = 1;  % Terminate the integration
+    direction = 0;   % Detect events in either direction
 end
 
